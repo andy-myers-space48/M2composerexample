d48c842e53160f4306b5049d820fe9f4c70d3bc3
Andy <andy.myers@space48.com>
2017-03-09 10:35:47 +0000

    Init Commit

---
+++ Model/Example.php
@@ -0,0 +1,6 @@
+<?php
+namespace AndyMyersSpace48\M2composerexample\Model;
+class Example
+{
+    # Empty Example file
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'AndyMyersSpace48_M2composerexample',
+        __DIR__
+    );
